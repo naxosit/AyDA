8dbb0fd6261551096a51d5cd63e99e3105ea0b2c
naxosit <128820710+naxosit@users.noreply.github.com>
2024-06-08 13:37:36 -0400

    Se añade el código del 'Juego De Las 8 Reinas' en lenguaje Python.

---
+++ Proyecto AyDA/Algoritmos Voraces (Greedy)/JuegoDeLas8Reinas.py
@@ -0,0 +1,18 @@
+# Este programa es una traducción de la solución de Nilaus Wirth's en el
+# lenguaje de programación de Python, pero no tiene el índice aritmético
+# encontrado en el original y en su lugar utiliza listas para mantener el
+# código del programa lo más simple posible. 
+
+def Reinas(n: int, i: int, a: list, b: list, c: list):
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from Reinas(n, i + 1, a + [j], b + [i + j], c + [i - j])
+            else:
+                yield a
+
+for solucion in Reinas(8, 0, [], [], []):
+    print(solucion)
+
+
+# Código extraído de https://en.wikipedia.org/wiki/Eight_queens_puzzle#Sample_program
