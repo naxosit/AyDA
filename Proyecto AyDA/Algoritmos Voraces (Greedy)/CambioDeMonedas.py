9e2dacc8e0e334ba4e60300746d815b22f806ae8
NAS-one <nomepillai@gmail.com>
2024-06-08 23:39:25 -0400

    Codigo casi final

--- Proyecto AyDA/Algoritmos Voraces (Greedy)/CambioDeMonedas.py
+++ Proyecto AyDA/Algoritmos Voraces (Greedy)/CambioDeMonedas.py
@@ -1,29 +1,28 @@
-def minimo_monedas(monedas, P):
-    # ordena las monedas en orden descendente
-    monedas.sort(reverse=True)
-    
-    # número total de monedas inicial
-    total_monedas = 0
-    
-    # Crea una lista que almacena el número de todas las monedas usadas
-    num_monedas = [0] * len(monedas)
-    
-    # Recorre todas las monedas disponibles comenzando por la de mayor valor
-    for i, moneda in enumerate(monedas):
-        # Si el valor de la moneda es menor o igual que la cantidad P
-        if P >= moneda:
-            # Calcula cuantas monedas que cumplen se pueden usar
-            num = P // moneda
-            
-            # Actualiza el total de monedas
-            total_monedas += num
-            
-            # Almacena el número de monedas de este tipo usadas
-            num_monedas[i] = num
+#Definimos los valores de las monedas chilenas a usar
+monedas = [10, 50, 100, 500]
 
+#i es un índice que comienza desde el final de la lista 'monedas'
+i = len(monedas) - 1
 
-###incompleto aun falta
-#https://www.geeksforgeeks.org/greedy-algorithm-to-find-minimum-number-of-coins/
-#https://www.codespeedy.com/collect-all-coins-in-minimum-number-of-steps-in-greedy-method-in-python/
-#code de Dipam Hazra
+#Cantidad a cambiar
+P = 2550
+#Lista donde almacenaremos las monedas que usaremos para el cambio
+total_monedas = []
 
+# Mientras 'i' sea mayor o igual a 0, es decir, mientras no hayamos recorrido todas las monedas
+while(i >= 0):
+    # Si el valor de la moneda actual es menor o igual que P
+    if (P >= monedas[i]):
+          # Mientras el valor de la moneda actual sea menor o igual que P
+        while (P >= monedas[i]):
+            #Añadimos la moneda actual a total_monedas
+            total_monedas.append(monedas[i])
+            #Restamos el valor actual a P
+            P -= monedas[i]
+    #Decrementamos i para pasar a la siguiente moneda
+    i -= 1
+
+print(f'Total monedas usadas {len(total_monedas)} : ', sep=",")
+print(*total_monedas)
+##https://github.com/MyloTuT/Greedy
+##https://github.com/Kashish24/hackerEarthCodingSolutions/blob/main/greedy-algorithm-to-find-minimum-number-of-coins.py
