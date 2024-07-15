0f0d3eeb6f161f025806aedbd674b70bd67ebf9c
Torreta134 <Miya134@users.noreply.github.com>
2024-07-14 22:13:25 -0400

    Actualizacion sudoku greedy e backtracking

--- Proyecto AyDA/Backtracking/SudokuB.py
+++ Proyecto AyDA/Backtracking/SudokuB.py
@@ -1,11 +1,15 @@
+import psutil
+import os
+import time
+
 # Programa de backtracking en Python para resolver el problema de Sudoku
 
 # Función de utilidad para imprimir el tablero
 def print_grid(arr):
-    for i in range(9):
-        for j in range(9):
-            print(arr[i][j], end=" ")
-        print()
+    for i in range(9):  # O(N)
+        for j in range(9):  # O(N)
+            print(arr[i][j], end=" ")  # O(1)
+        print()  # O(1)
 
 # Función para encontrar una celda no asignada en el tablero
 # Busca en el tablero para encontrar una celda no asignada.
@@ -15,38 +19,38 @@ def print_grid(arr):
 # 'l' es una lista que se pasa desde la función solve_sudoku
 # para llevar un registro de la fila y la columna.
 def find_empty_location(arr, l):
-    for row in range(9):
-        for col in range(9):
-            if arr[row][col] == 0:
-                l[0] = row
-                l[1] = col
-                return True
-    return False
+    for row in range(9):  # O(N)
+        for col in range(9):  # O(N)
+            if arr[row][col] == 0:  # O(1)
+                l[0] = row  # O(1)
+                l[1] = col  # O(1)
+                return True  # O(1)
+    return False  # O(1)
 
 # Devuelve un booleano que indica si alguna entrada asignada
 # en la fila especificada coincide con el número dado.
 def used_in_row(arr, row, num):
-    for i in range(9):
-        if arr[row][i] == num:
-            return True
-    return False
+    for i in range(9):  # O(N)
+        if arr[row][i] == num:  # O(1)
+            return True  # O(1)
+    return False  # O(1)
 
 # Devuelve un booleano que indica si alguna entrada asignada
 # en la columna especificada coincide con el número dado.
 def used_in_col(arr, col, num):
-    for i in range(9):
-        if arr[i][col] == num:
-            return True
-    return False
+    for i in range(9):  # O(N)
+        if arr[i][col] == num:  # O(1)
+            return True  # O(1)
+    return False  # O(1)
 
 # Devuelve un booleano que indica si alguna entrada asignada
 # dentro del cuadro 3x3 especificado coincide con el número dado.
 def used_in_box(arr, row, col, num):
-    for i in range(3):
-        for j in range(3):
-            if arr[i + row][j + col] == num:
-                return True
-    return False
+    for i in range(3):  # O(1)
+        for j in range(3):  # O(1)
+            if arr[i + row][j + col] == num:  # O(1)
+                return True  # O(1)
+    return False  # O(1)
 
 # Comprueba si será legal asignar num a la fila y columna dadas.
 # Devuelve un booleano que indica si será legal asignar
@@ -54,9 +58,9 @@ def used_in_box(arr, row, col, num):
 def check_location_is_safe(arr, row, col, num):
     # Comprueba si 'num' no está ya colocado en la fila actual,
     # columna actual y cuadro 3x3 actual
-    return (not used_in_row(arr, row, num) and
-            not used_in_col(arr, col, num) and
-            not used_in_box(arr, row - row % 3, col - col % 3, num))
+    return (not used_in_row(arr, row, num) and  # O(N)
+            not used_in_col(arr, col, num) and  # O(N)
+            not used_in_box(arr, row - row % 3, col - col % 3, num))  # O(1)
 
 # Toma un tablero parcialmente completado y
 # trata de asignar valores a todas las ubicaciones no asignadas
@@ -67,38 +71,38 @@ def solve_sudoku(arr):
     # 'l' es una lista que mantiene el
     # registro de la fila y columna en la
     # función find_empty_location
-    l = [0, 0]
+    l = [0, 0] # O(1)
     
     # Si no hay ubicación no asignada, hemos terminado
-    if not find_empty_location(arr, l):
-        return True
+    if not find_empty_location(arr, l): # O(N^2)
+        return True  # O(1)
     
     # Asignando valores de lista a fila y columna
     # que obtuvimos de la función anterior
-    row = l[0]
-    col = l[1]
+    row = l[0]  # O(1)
+    col = l[1]  # O(1)
     
     # Considera dígitos del 1 al 9
-    for num in range(1, 10):
+    for num in range(1, 10):  # O(1)
         # Si parece prometedor
-        if check_location_is_safe(arr, row, col, num):
+        if check_location_is_safe(arr, row, col, num):  # O(N)
             # Hace una asignación tentativa
-            arr[row][col] = num
+            arr[row][col] = num  # O(1) 
 
             # Devuelve True si tiene éxito
-            if solve_sudoku(arr):
-                return True
+            if solve_sudoku(arr):   # O(9^(N^2))
+                return True  # O(1)
 
             # En caso de fallo, deshace la asignación e intenta de nuevo
-            arr[row][col] = 0
+            arr[row][col] = 0  # O(1)
     
     # Esto dispara el backtracking
-    return False
+    return False  # O(1)
 
 # Función principal para probar las funciones anteriores
 if __name__ == "__main__":
     # Creando un array 2D para el tablero
-    grid = [[0 for x in range(9)] for y in range(9)]
+    grid = [[0 for x in range(9)] for y in range(9)]  # O(N^2)
     
     # Asignando valores al tablero
     grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
@@ -111,11 +115,24 @@ if __name__ == "__main__":
             [0, 0, 0, 0, 0, 0, 0, 7, 4],
             [0, 0, 5, 2, 0, 6, 3, 0, 0]]
     
+    process = psutil.Process(os.getpid())  
+    start_memory = process.memory_info().rss  
+    start_time = time.time()
+
     # Si tiene éxito, imprime el tablero
-    if solve_sudoku(grid):
-        print_grid(grid)
+    if solve_sudoku(grid): # O(9^(N^2))
+        print_grid(grid)  #O(N^2)
     else:
-        print("No solution exists")
+        print("No solution exists") 
+
+    end_memory = process.memory_info().rss  
+    end_time = time.time()  
+    
+    print(f"Memory used: {end_memory - start_memory} bytes")  
+    print(f"Execution time: {end_time - start_time} seconds")  
+
+    #COMPLEJIDAD ALGORITMICA # O(9^(N^2))  n=9
+                            #O(9^81)
 
     #https://www.geeksforgeeks.org/sudoku-backtracking-7/
 	# Este codigo es contribuido por sudhanshgupta2019a y modificado por nosotros
--- Proyecto AyDA/Greedy/Sudoku.py
+++ Proyecto AyDA/Greedy/Sudoku.py
@@ -1,61 +1,65 @@
+import psutil
+import os
+import time
+
 #N es el tamaño de la matriz N*N
 N = 9
 
 # funcion de utilidad para imprimiar la matriz
 def printing(arr):
-	for i in range(N):
-		for j in range(N):
-			print(arr[i][j], end = " ")
-		print()
+	for i in range(N): # O(N)
+		for j in range(N): # O(N)
+			print(arr[i][j], end = " ") # O(1)
+		print() # O(1)
 
 #Verifica si es legal asignar un número dado a una fila y columna específicas
 
 def isSafe(grid, row, col, num):
 
 	# Verifica fila
-	for x in range(9):
-		if grid[row][x] == num:
-			return False
+	for x in range(9): # O(N)
+		if grid[row][x] == num: # O(1)
+			return False # O(1)
 
 	# Verifica la columna
-	for x in range(9):
-		if grid[x][col] == num:
-			return False
-
+	for x in range(9): # O(N)
+		if grid[x][col] == num: # O(1)
+			return False # O(1)
+ 
 	# Verifica cuadricula 3x3
-	startRow = row - row % 3
-	startCol = col - col % 3
-	for i in range(3):
-		for j in range(3):
-			if grid[i + startRow][j + startCol] == num:
-				return False
-	return True
+	startRow = row - row % 3 # O(1)
+	startCol = col - col % 3 # O(1)
+	for i in range(3): # O(1)
+		for j in range(3): # O(1)
+			if grid[i + startRow][j + startCol] == num: # O(1)
+				return False # O(1)
+	return True # O(1)
 
 #Encuentra las posiciones vacias y las ordena segun el nro de posibles nros que pueden ser colocados en esas posiciones
-def findEmpty(grid):
-	empty_positions = []
-	for row in range(N):
-		for col in range(N):
-			if grid[row][col] == 0:
-				possible_numbers = [num for num in range(1, N+1) if isSafe(grid, row, col, num)]
-				empty_positions.append((row, col, possible_numbers))
-	empty_positions.sort(key=lambda x: len(x[2]))
-	return empty_positions
+def findEmpty(grid):  # O(1)
+	empty_positions = []  # O(N)
+	for row in range(N):  # O(N)
+		for col in range(N): # O(N)
+			if grid[row][col] == 0:  # O(1)
+				possible_numbers = [num for num in range(1, N+1) if isSafe(grid, row, col, num)] # O(N^2)
+				empty_positions.append((row, col, possible_numbers)) # O(1)
+	empty_positions.sort(key=lambda x: len(x[2])) # O(N^2 log N^2)
+	return empty_positions # O(1)
 
 #Intenta asignar valores a todas las ubicaciones no asignadas de tal manera que cumpla 
 #con los requisitos para la solución de Sudoku (no duplicación en filas, columnas y cajas)
 def solveSudokuGreedy(grid):
-	empty_positions = findEmpty(grid)
-	if not empty_positions:
-		return True
-	row, col, possible_number = empty_positions[0]
-	for num in possible_number:
-		if isSafe(grid, row, col, num):
-			grid[row][col] = num
-			if solveSudokuGreedy(grid):
-				return True
-			grid[row][col] = 0
-	return False
+    empty_positions = findEmpty(grid)  # O(N^3)
+    if not empty_positions:  # O(1)
+        return True  # O(1)
+    row, col, possible_number = empty_positions[0]  # O(1)
+    for num in possible_number:  # O(N)
+        if isSafe(grid, row, col, num):  # O(N)
+            grid[row][col] = num  # O(1)
+            if solveSudokuGreedy(grid):  # O(N^5)
+                return True  # O(1)
+            grid[row][col] = 0  # O(1)
+    return False  # O(1)
 
 
 # Driver Code
@@ -71,10 +75,24 @@ grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 7, 4],
 		[0, 0, 5, 2, 0, 6, 3, 0, 0]]
 
+process = psutil.Process(os.getpid())
+start_memory = process.memory_info().rss
+start_time = time.time()
+
+
 if (solveSudokuGreedy(grid)):
 	printing(grid)
 else:
 	print("no solution exists ")
 
+end_time = time.time()
+end_memory = process.memory_info().rss
+
+print(f"Memory used: {end_memory - start_memory} bytes")
+print(f"Execution time: {end_time - start_time} seconds")
+
+
+	#COMPLEJIDAD ALGORITMICA O(N^5)
+	
     #https://www.geeksforgeeks.org/sudoku-backtracking-7/
 	# Este codigo es contribuido por sudhanshgupta2019a y modificado por nosotros
